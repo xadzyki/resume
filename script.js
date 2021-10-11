93f7717be8991b2af24ea74059b9eea2da14dfd6
xadzyki <xadzyki@yandex.ru>
2021-10-11 15:42:19 +0500

    scroll up

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elms = document.querySelectorAll('.slider');
-    for (var i = 0, len = elms.length; i < len; i++) {
-      // инициализация elms[i] в качестве слайдера
-      new ChiefSlider(elms[i]);
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var elms = document.querySelectorAll(".slider");
+  for (var i = 0, len = elms.length; i < len; i++) {
+    // инициализация elms[i] в качестве слайдера
+    new ChiefSlider(elms[i]);
+  }
+});
+
+var smoothJumpUp = function () {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    window.scrollBy(0, -50);
+    setTimeout(smoothJumpUp, 10);
+  }
+};
+
+// window.onscroll = function () {
+//   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+//   if (scrolled > 100) {
+//     document.getElementById("upbutton").style.display = "block";
+//   } else {
+//     document.getElementById("upbutton").style.display = "none";
+//   }
+// };

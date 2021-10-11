879d89725789701eaf9381985cfad5399c1094a3
xadzyki <xadzyki@yandex.ru>
2021-10-11 16:49:00 +0500

    спуск и поднятие на вверх

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-var smoothJumpUp = function () {
+let smoothJumpUp = function () {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     window.scrollBy(0, -50);
     setTimeout(smoothJumpUp, 10);
   }
 };
+
+let smothJumpSection = function (count) {
+  // window.innerHeight;
+  if (
+    document.body.scrollTop > 0 ||
+    document.documentElement.scrollTop < count
+  ) {
+    // scroll= scroll+40;
+    window.scrollBy(0, 40);
+    setTimeout(smothJumpSection(count), 10);
+    console.log(document.body.scrollTop);
+    console.log(document.documentElement.scrollTop);
+  }
+}
+
+// let smothJumpContact = function () {
+//   // window.innerHeight;
+
+//  if(document.body.scrollTop > 0 || document.documentElement.scrollTop < 1650){
+//     window.scrollBy(0, 40);
+//     setTimeout(smothJumpContact, 10);
+//     console.log(document.body.scrollTop);
+//     console.log(document.documentElement.scrollTop);
+//  }
+// };
 
 // window.onscroll = function () {
 //   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
